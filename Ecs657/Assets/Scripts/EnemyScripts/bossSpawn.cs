7d358f28a3fc5554eeecc7d4dfbffab447ba9c95
Remy-Macdermott <71390314+Remy-Macdermott@users.noreply.github.com>
2024-01-07 23:21:19 +0000

    added boss cutscene
    shows player when the boss has spawned

--- Ecs657/Assets/Scripts/EnemyScripts/bossSpawn.cs
+++ Ecs657/Assets/Scripts/EnemyScripts/bossSpawn.cs
@@ -1,32 +1,60 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossSpawner : MonoBehaviour
 {
     public GameObject bossPrefab; // The boss GameObject you want to spawn
+    public GameObject bossCamera; // Reference to the boss camera
+    public GameObject playerCamera; // Reference to the player camera
     public float bossSpawnTime = 300.0f; // 5 minutes in seconds
     [SerializeField] private Timer timer;
     [SerializeField] private Transform spawnPoint;
 
-    public bool bossSpawned = false;
+    private bool bossSpawned = false;
 
     void Start()
     {
         // Find the Timer component and assign it to the 'timer' variable
         timer = GameObject.Find("Timer").GetComponent<Timer>();
+
+        // Ensure the initial camera state is correct
+        SwitchCamera(playerCamera);
     }
 
     void Update()
     {
         // Check if the timer has reached the specified time and the boss has not been spawned yet
-        if (timer != null && timer.totalTimePassed <= bossSpawnTime && !bossSpawned)
+        if (timer != null && timer.totalTimePassed >= bossSpawnTime && !bossSpawned)
         {
             // Set a flag to prevent spawning more than once
             bossSpawned = true;
+
+            // Switch to the boss camera
+            SwitchCamera(bossCamera);
+
+            // Spawn the boss GameObject
             SpawnBoss();
-            Destroy(gameObject);
+            
+
+            // Delay for some time and switch back to the player camera
+            StartCoroutine(SwitchBackToPlayerCamera(3f)); // Adjust the time as needed
+            
+            //Destroy(gameObject);
         }
+    }
+
+    void SwitchCamera(GameObject cameraToActivate)
+    {
+        playerCamera.SetActive(false);
+        cameraToActivate.SetActive(true);
+    }
+
+    IEnumerator SwitchBackToPlayerCamera(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        // Switch back to the player camera
+        SwitchCamera(playerCamera);
     }
 
     void SpawnBoss()
